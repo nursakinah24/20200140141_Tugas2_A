293c45c03f7e0d51b0db8f3d7aff3835d25b5e9a
nursakinah24 <nursakinah2402@gmail.com>
2022-03-24 10:02:46 +0700

    Add project files.

---
+++ Tugas2_20200140141_A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas2_20200140141_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
